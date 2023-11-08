7aff4fc56c1392be2d5b486e0e942d4b834c0055
gulizaer <146580308+gulizaer@users.noreply.github.com>
2023-11-08 04:12:44 +0100

    day08
    tasks

---
+++ Loans.java
@@ -0,0 +1,28 @@
+public class Loans {
+    public static void main(String[] args) {
+
+        int Salary = 85000,
+            CreditScore = 700;
+
+        String result = (Salary >= 60000 && CreditScore >=650) ? "Loan Approved" : "Loan Denied";
+
+        System.out.println(result);
+
+
+
+
+
+      /*
+      Create a class called Loans, Given two variables salary and credit score,
+      use those given info to determine if you can get a loan.
+
+            To be approve for the loan, print: "Loan Approved":
+            Salary: above 60,000
+            Credit Score: above 650
+            Otherwise print: "Loan Denied"
+
+              NOTE: MUST USE TERNARY.
+       */
+
+    }
+}
---
+++ NumberToworld.java
@@ -0,0 +1,33 @@
+public class NumberToworld {
+    public static void main(String[] args) {
+
+   int number = 9;
+   String result = "Invalid";
+
+   if (number>=0 && number <= 9) {
+    result = (number == 0) ? "Zero" : (number == 1) ? "One" : (number == 2) ? "Two" : (number == 3) ? "Tree"
+    : (number == 4) ? "Four" : (number == 5) ? "Five" : (number == 6) ? "Sex" : (number == 7) ? "Seven"
+    : (number == 8) ? "Eight" : "Nine";}
+
+        System.out.println(result);
+
+
+
+
+
+/*
+ Create a class called NumberToWord,
+		write a java program that can convert numbers between 0 ~ 9 to words
+			Ex:
+				number = 1;
+
+			output:
+				One
+
+		Note: MUST use ternary
+ */
+
+
+    }
+
+}
